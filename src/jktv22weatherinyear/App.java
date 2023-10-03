e2446900e0f02f0d4ffa1016751c97a3f13e97ab
pupil <pupil@ivkhk.ee>
2023-10-03 10:37:29 +0300

    начало проекта

---
+++ src/jktv22weatherinyear/App.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv22weatherinyear;
+import java.util.Scanner;
+/**
+ *
+ * @author pupil
+ */
+public class App {
+    private final Scanner scanner;
+
+    public App() { /*insert code -> constructor*/
+        scanner = new Scanner(System.in);
+    }
+    
+
+    public void run() {
+        System.out.println("----- Weather in year -----");
+        boolean repeat = true;
+        do {
+            System.out.println("задачу");
+            System.out.println("0.Закончить программу");
+            System.out.println("1.Погода в указанную дату");
+            System.out.println("2.Дни в которые былла самая тёплая и холодная погода");
+            System.out.println("3.Средняя температура за указанный месяц");
+            System.out.println("4.Средняя температура по всем месяцам");
+            System.out.println("Выберите номер задачи: ");
+            int task = scanner.nextInt();scanner.nextLine();
+            switch (task) { /*sw-> tab*/
+                case 0:
+                    System.out.println("Выбран выход из программы");
+                    repeat = false;
+                    break;
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                default:
+                    System.out.println("Выберите номер из списка");  
+                    }  
+                }while (repeat);
+                System.out.println("--- end ---");
+                
+    }
+    
+}
